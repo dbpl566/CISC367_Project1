06dc3593b699b24385dc299d232a6ea0b9db93b4
dbpl566 <54877378+dbpl566@users.noreply.github.com>
2021-09-28 13:40:01 -0400

    Add files via upload

--- driver.py
+++ driver.py
@@ -1,8 +1,19 @@
+import csv
 import input_file
 import analysis
 from input_file import data
-from analysis import checkUrls
+from analysis import checkUrls, fields, rows
 
 #print(data) #testing inputfile output data in list
 
 checkUrls(data)
+
+#spreadsheet name
+filename = "chat_analysis_output.csv"
+
+#write to csv
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)
+    csvwriter.writerows(rows)
+
